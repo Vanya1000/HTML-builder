d8d57de889d46077d77d881550b3c314086a8995
Vanya1000 <Vanya1000@yandex.ru>
2022-05-20 13:13:26 +0300

    fix: task 5 line break in css bundle

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,26 @@
 const { stdout } = process;
 const path = require('path');
-const { readdir, writeFile } = require('fs/promises');
+const { readdir, writeFile, stat } = require('fs/promises');
 const fs = require('fs');
 
 const sourcePath = path.join(__dirname, 'styles');
 const targetPath = path.join(__dirname, 'project-dist');
-const data = [];
 
 async function createBundle(sourse, target) {
   try {
     const files = await readdir(sourse, { withFileTypes: true });
+    const data = [];
     for (const file of files) {
       const extFile = path.extname(path.join(sourse, file.name));
       if (file.isFile() && extFile === '.css') {
-        const input = fs.createReadStream(path.join(sourse, file.name), 'utf-8');
+        const size = await stat(path.join(sourse, file.name));
+        const input = fs.createReadStream(path.join(sourse, file.name), { highWaterMark: size.size }, 'utf-8');
         for await (const chunk of input) {
           data.push(chunk);
         }
       }
     }
-    await writeFile(path.join(target, 'bundle.css'), data.join(''), 'utf8');
+    await writeFile(path.join(target, 'bundle.css'), data.join('\n'), 'utf8');
   } catch (err) {
     stdout.write(`\nError: ${err.message}\n`);
   }
